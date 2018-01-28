324f1317c4134a45e3e866a2c803333dc323cac1
Fan Yang <missdeer@dfordsoft.com>
2018-01-28 18:24:00 +0800

    (*)fix character encoding of command line arguments

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 	"strings"
 	"unicode/utf8"
 
+	"github.com/dfordsoft/golib/ic"
+
 	"github.com/dfordsoft/golib/filter"
 	"github.com/dfordsoft/golib/semaphore"
 	"github.com/elazarl/goproxy"
@@ -105,7 +107,11 @@ func main() {
 	semaImage = semaphore.New(opts.Parallel * 10)
 	semaArticle = semaphore.New(opts.Parallel)
 	semaPDF = semaphore.New(opts.Parallel)
-	titleFilter = filter.Filter(opts.Filter)
+	if runtime.GOOS == "windows" {
+		titleFilter = filter.Filter(ic.ConvertString("gbk", "utf-8", opts.Filter))
+	} else {
+		titleFilter = filter.Filter(opts.Filter)
+	}
 
 	if err := setCA(opts.CaCert, opts.CaKey); err != nil {
 		log.Fatalln(err)
