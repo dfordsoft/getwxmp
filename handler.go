78478e6c5541f683c7a6153705aef5c221351beb
Fan Yang <me@minidump.info>
2018-02-04 21:32:43 +0800

    (*)larger interval

--- handler.go
+++ handler.go
@@ -168,7 +168,7 @@ func getArticleList() {
 		if m.CanMsgContinue == 0 {
 			break
 		}
-		time.Sleep(time.Duration(rand.Intn(4000)+1000) * time.Millisecond)
+		time.Sleep(time.Duration(rand.Intn(10000)+5000) * time.Millisecond)
 	}
 
 	if strings.ToLower(opts.Format) == "mobi" {
